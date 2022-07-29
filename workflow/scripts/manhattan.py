bbf870c33cfd421ffa91785abe0307f70f97f019
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2022-07-29 07:05:03 -0700

    merge manhattan plots into a single plot

--- workflow/scripts/manhattan.py
+++ workflow/scripts/manhattan.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import re
 import sys
 import click
 import numpy as np
@@ -14,10 +15,11 @@ from pandas.api.types import CategoricalDtype
 @click.option(
     "-l",
     "--linear",
+    multiple=True,
     type=click.Path(exists=True, path_type=Path),
-    default=Path("-"),
+    default=[Path("-")],
     show_default="stdin",
-    help="A PLINK2 .linear file containing the assocation results",
+    help="PLINK2 .linear files containing the assocation results",
 )
 @click.option(
     "-o",
@@ -27,7 +29,7 @@ from pandas.api.types import CategoricalDtype
     show_default="stdout",
     help="A PNG file containing a Manhattan plot of the results",
 )
-def main(linear=sys.stdin, output=sys.stdout):
+def main(linear=[sys.stdin], output=sys.stdout):
     """
     Create a manhattan plot from the results of a PLINK2 GWAS
     """
@@ -47,42 +49,51 @@ def main(linear=sys.stdin, output=sys.stdout):
     }
     keep_cols = ["chromosome", "pos", "beta", "se", "pval"]
 
-    # parse the .linear file
-    df = pd.read_csv(
-        linear,
-        sep="\t",
-        header=0,
-        names=plink_cols.values(),
-        usecols=keep_cols,
-    )
-    # -log_10(pvalue)
-    df['-10log10(p)'] = -10*np.log(df["pval"])
-    df.chromosome = df.chromosome.astype('category')
-    df.chromosome = df.chromosome.astype(
-        CategoricalDtype(sorted(map(int, df.chromosome.dtype.categories)), ordered=True)
-    )
-    df = df.sort_values('chromosome')
+    # create the plot
+    fig, ax = plt.subplots(1, len(linear), figsize=(14, 8), sharex=True, sharey=True)
 
-    # How to plot gene vs. -log10(pvalue) and colour it by chromosome?
-    df_grouped = df.groupby(('chromosome'))
-    fig, ax = plt.subplots(1, 2, figsize=(14, 8))
+    # parse the .linear files
+    dfs = {}
+    max_pval = -1
+    for idx, linear_fname in enumerate(linear):
+        df = pd.read_csv(
+            linear_fname,
+            sep="\t",
+            header=0,
+            names=plink_cols.values(),
+            usecols=keep_cols,
+        )
+        # throw out anything outside of a 1 Mbp window around the peak
+        width = 1e6 / 3
+        max_idx = min(df.index, key=lambda i: df.iloc[i]["pval"])
+        max_pos = df.iloc[max_idx]["pos"]
+        df = df[(df["pos"] > max_pos-width) & (df["pos"] < max_pos+width)]
+        # -log_10(pvalue)
+        df['-log10(p)'] = -np.log(df["pval"])
+        df.chromosome = df.chromosome.astype('category')
+        df.chromosome = df.chromosome.astype(
+            CategoricalDtype(sorted(map(int, df.chromosome.dtype.categories)), ordered=True)
+        )
+        df = df.sort_values('chromosome')
+        # create the plot using pandas and add it to the figure
+        df.plot(kind='scatter', x='pos', y='-log10(p)', ax=ax[idx])
+        ax_name = Path(Path(linear_fname.stem).stem).stem
+        ax[idx].title.set_text(ax_name.replace('-', " + "))
+        ax[idx].set(xlabel=None, ylabel=None)
+        dfs[ax_name] = df
+        max_val = ax[idx].get_ylim()[1]
+        if max_pval < max_val:
+            max_pval = max_val
+    df = pd.concat(dfs, ignore_index=True)
 
-    # qqplot
-    sm.qqplot(df['pval'], ax=ax[0], line='45')
+    # set the y-axis limit so that both axes have the same limit
+    print(max_pval)
+    for idx, linear_fname in enumerate(linear):
+        ax[idx].set_ylim(top=max_pval)
 
-    # manhattan plot
-    x_labels = []
-    for num, (name, group) in enumerate(df_grouped):
-        group.plot(kind='scatter', x='pos', y='-10log10(p)', ax=ax[1])
-        x_labels.append(name)
-    ax[1].set_xticklabels(x_labels)
-    # set axis limits
-    ax[1].set_xlim([0, max(df['pos'])])
-    # ax[1].set_ylim([0, 3.5])
-    # x axis label
-    ax[1].set_xlabel('Chromosome')
-
-    # show the graph
+    # save the graph
+    fig.supxlabel('Chromosomal Position')
+    fig.supylabel('-log10(p value)')
     plt.savefig(output)
 
 
