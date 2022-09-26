76c54543b7743a3840641492f2010d37f7b8fc4b
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2022-09-26 13:36:34 -0700

    account for potentially duplicated IDs in merge script

--- workflow/scripts/merge.py
+++ workflow/scripts/merge.py
@@ -10,10 +10,18 @@ from haptools.data import GenotypesPLINK
 @click.argument("file1", type=click.Path(exists=True, path_type=Path))
 @click.argument("file2", type=click.Path(exists=True, path_type=Path))
 @click.argument("output", type=click.Path(path_type=Path))
-def main(file1: Path, file2: Path, output: Path):
+@click.option(
+    "--replace/--no-replace",
+    is_flag=True,
+    default=True,
+    show_default=True,
+    help="Whether to use the variants in file2 to replace those in file1",
+)
+def main(file1: Path, file2: Path, output: Path, replace: bool = True):
     """
     Merge variants from two PGEN files that have the same set of samples
 
+    \f
     Parameters
     ----------
     file1: Path
@@ -22,10 +30,16 @@ def main(file1: Path, file2: Path, output: Path):
         The path to the second pgen file
     output: Path
         The path to the output pgen file
+    replace: bool, optional
+        If True, take the left-inner join of the set of variants, so that
+        conflicting IDs from file2 are used to replace the GTs in file1
+
+        You should only set this flag to False if you are confident that file1 and
+        file2 have no conflicting variant IDs. Otherwise, you may end up with an
+        output fileset that has duplicate IDs!
     """
     gts1 = GenotypesPLINK(file1)
     gts2 = GenotypesPLINK(file2)
-    out = GenotypesPLINK(output)
 
     gts1.read()
     gts2.read()
@@ -35,11 +49,26 @@ def main(file1: Path, file2: Path, output: Path):
 
     assert gts1.samples == gts2.samples
 
-    out.samples = gts1.samples
-    out.variants = np.concatenate((gts1.variants, gts2.variants))
-    out.data = np.concatenate((gts1.data, gts2.data), axis=1)
+    if replace:
+        # which variants are shared? what are their indices within each file?
+        common_ids, idxs_in_1, idxs_in_2 = np.intersect1d(
+            gts1.variants["id"],
+            gts2.variants["id"],
+            return_indices=True
+        )
+        # update variants in file1 with their counterparts in file2
+        gts1.variants[idxs_in_1] = gts2.variants[idxs_in_2]
+        gts1.data[:, idxs_in_1, :] = gts2.data[:, idxs_in_2, :]
+        # remove the common variants from file2
+        gts2.variants = np.delete(gts2.variants, idxs_in_2)
+        gts2.data = np.delete(gts2.data, idxs_in_2, axis=1)
 
-    out.write()
+    # append any variants from file2 to the end of file1
+    gts1.variants = np.concatenate((gts1.variants, gts2.variants))
+    gts1.data = np.concatenate((gts1.data, gts2.data), axis=1)
+
+    gts1.fname = output
+    gts1.write()
     
 
 if __name__ == "__main__":
